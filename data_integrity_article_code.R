bdfa46429768277af7794258e43da31fae9d3fb5
Jon Mattes <jomattes@users.noreply.github.com>
2019-12-05 12:32:43 -0800

    Add comments

--- data_integrity_article_code.R
+++ data_integrity_article_code.R
@@ -3,15 +3,18 @@ library(dplyr)
 
 set.seed(111)
 
+# create no error dates
 date_seq <- seq.Date(as.Date('2019-07-01'), as.Date('2019-09-30'), 'day')
 date_seq <- as.data.frame(rep(date_seq, 6000))
 colnames(date_seq) <- 'Visit_Date'
 
+# create no error wait times
 wait_times_normal <- as.data.frame(rnorm(92 * 6000, mean=30, sd=5))
 colnames(wait_times_normal) <- 'Wait_Time'
 
 combo_df <- cbind(date_seq, wait_times_normal)
 
+# create no error plot
 g1 <- combo_df %>% 
   group_by(Visit_Date) %>% 
   summarize(AVG_Wait_Time = mean(Wait_Time)) %>% 
@@ -27,10 +30,11 @@ g1 <- combo_df %>%
   ggtitle('Average Wait Time per Day (No Error)') +
   theme(plot.title = element_text(size = 20, face = "bold"))
 
-
+# add error value
 new_combo_df <- rbind(combo_df, c('2019-09-30', 781))
 new_combo_df$Wait_Time <- as.numeric(new_combo_df$Wait_Time)
 
+# create error plot
 g2 <- new_combo_df %>% 
   group_by(Visit_Date) %>% 
   summarize(AVG_Wait_Time = mean(Wait_Time)) %>% 
